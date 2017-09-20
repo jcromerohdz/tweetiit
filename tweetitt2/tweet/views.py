b83b5c6a58fb0ad10e48a331b07d3183fb1a86ef
Christian <jcromerohdz@gmail.com>
2017-09-20 16:44:45 -0700

    tweet list and detail view

--- tweetitt2/tweet/views.py
+++ tweetitt2/tweet/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Tweet
 
 # Create your views here.
 
@@ -10,3 +11,14 @@ def home_tweet(request):
     v = "Desde home tweet"
     v2 = {"Usuario":"Christian"}
     return render(request, "home.html", {'v':v, 'v2':v2})
+
+#CRUD Create Retrive or Read Update Delete
+
+# Retrive GET desde la base de datos
+def tweet_detail_view(request):
+    msg = "vista de detalle"
+    return render(request, "tweets/detail_view.html", {'msg':msg})
+
+def tweet_list_view(request):
+    msg = "lista de tweets"
+    return render(request, "tweets/list_view.html", {'msg':msg})
